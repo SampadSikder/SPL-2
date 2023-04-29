d5504fca5c8656f6482e71b314b51bde62c3413f
Sadia Tabassum <60074200+SadiaTabassum1216@users.noreply.github.com>
2023-04-29 15:41:15 +0600

    watchlist

--- stockTech_frontend/src/app/components/watchlist/watchlist.component.ts
+++ stockTech_frontend/src/app/components/watchlist/watchlist.component.ts
@@ -17,197 +17,119 @@ export class WatchlistComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   dataAvail = true;
   public spinner: boolean = true;
-  list: company[] = [];
+  list: any;
 
 
 
 
   ngOnInit(): void {
     this.renderDataTable();
-    this.receiveMarketData().subscribe((data) => {
-      this.list = data;
-      data = data.filter(function (dat: any) {
-        return dat.trading_code != '';
-      });
-      this.spinner = false;
-      this.dtOptions.data = data;
-      this.dataAvail = true;
+    this.receiveMarketData()
+    .subscribe({
+      next: (res) => {
+       
+        this.list = res;
+        console.log(this.list);         
+      },
+      error: (e) => console.error(e)
     });
 
   }
 
-  receiveMarketData(): Observable<any> {
+  receiveMarketData(): Observable<company[]> {
     return this.MarketDataService.getWatchlist();
   }
 
+  public remove(company: company) {
+     alert(company.trading_code + " is removed.");
 
-
-  renderDataTable(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      searching: true,
-      language: {
-        searchPlaceholder: "Search...",
-        search: ""
-      },
-      columnDefs: [
-        { width: '30em', targets: [0, 1, 2, 3, 4] },
-        { orderable: false, targets: [5] },
-
-      ],
-      columns: [
-        {
-          title: 'CODE', data: 'trading_code',
-          render: function (data, type, row) {
-            row.trading_code = row.trading_code.replace(/ /g, '-');
-            if (type === 'display') {
-              if (row.change < 0) {
-                data =
-                  '<a style="color:red;" href="' +
-                  row.scrip +
-                  '/' +
-                  row.trading_code +
-                  '">' +
-                  data +
-                  '</a>';
-              } else if (row.change == 0) {
-                data =
-                  '<a style="color:#2a76e8;" href="' +
-                  row.scrip +
-                  '/' +
-                  row.trading_code +
-                  '">' +
-                  data +
-                  '</a>';
-              } else {
-                data =
-                  '<a style="color:green;" href="' +
-                  row.scrip +
-                  '/' +
-                  row.trading_code +
-                  '">' +
-                  data +
-                  '</a>';
-              }
-            }
-
-            return data;
-          },
-        },
-        { title: 'LTP', data: 'ltp' },
-        { title: 'CLOSEP', data: 'closep' },
-        {
-          title: 'CHANGE', data: 'change',
-          render: function (data, type, row) {
-            if (type === 'display') {
-              if (row.change < 0) {
-                data = '<span style="color:red;">' + data + '</span>';
-              } else if (row.change == 0) {
-                data = '<span style="color:#2a76e8;">' + data + '</span>';
-              } else {
-                data = '<span style="color:green;">' + data + '</span>';
-              }
-            }
-
-            return data;
-          },
-        },
-        { title: 'YCP', data: 'ycp' },
-        
-      ]
-    }
   }
 
 
+  renderDataTable(): void {
+   
+    this.dtOptions = {
+      lengthChange: false, 
+      pageLength: 20,
+      ordering:false
+     
+    }}
 
 
-// this.dtOptions = {
-//   lengthChange: false,
-//   columnDefs: [
-//     { width: '30em', targets: [0, 1, 2, 3, 4] },
-//     { orderable: false, targets: [5] },
-
-//   ],
-//    pageLength: 20,
 
 
-//   columns: [
-//     {
-//       title: 'CODE', data: 'trading_code',
-//       render: function (data, type, row) {
-//         row.trading_code = row.trading_code.replace(/ /g, '-');
-//         if (type === 'display') {
-//           if (row.change < 0) {
-//             data =
-//               '<a style="color:red;" href="' +
-//               row.scrip +
-//               '/' +
-//               row.trading_code +
-//               '">' +
-//               data +
-//               '</a>';
-//           } else if (row.change == 0) {
-//             data =
-//               '<a style="color:#2a76e8;" href="' +
-//               row.scrip +
-//               '/' +
-//               row.trading_code +
-//               '">' +
-//               data +
-//               '</a>';
-//           } else {
-//             data =
-//               '<a style="color:green;" href="' +
-//               row.scrip +
-//               '/' +
-//               row.trading_code +
-//               '">' +
-//               data +
-//               '</a>';
-//           }
-//         }
+      // columns: [
+      //   {
+      //     title: 'CODE', data: 'trading_code',
+      //     render: function (data, type, row) {
+      //       row.trading_code = row.trading_code.replace(/ /g, '-');
+      //       if (type === 'display') {
+      //         if (row.change < 0) {
+      //           data =
+      //             '<a style="color:red;" href="' +
+      //             row.scrip +
+      //             '/' +
+      //             row.trading_code +
+      //             '">' +
+      //             data +
+      //             '</a>';
+      //         } else if (row.change == 0) {
+      //           data =
+      //             '<a style="color:#2a76e8;" href="' +
+      //             row.scrip +
+      //             '/' +
+      //             row.trading_code +
+      //             '">' +
+      //             data +
+      //             '</a>';
+      //         } else {
+      //           data =
+      //             '<a style="color:green;" href="' +
+      //             row.scrip +
+      //             '/' +
+      //             row.trading_code +
+      //             '">' +
+      //             data +
+      //             '</a>';
+      //         }
+      //       }
 
-//         return data;
-//       },
-//     },
-//     { title: 'LTP', data: 'ltp' },
-//     { title: 'CLOSEP', data: 'closep' },
-//     { title: 'CHANGE', data: 'change',
-//       render: function (data, type, row) {
-//         if (type === 'display') {
-//           if (row.change < 0) {
-//             data = '<span style="color:red;">' + data + '</span>';
-//           } else if (row.change == 0) {
-//             data = '<span style="color:#2a76e8;">' + data + '</span>';
-//           } else {
-//             data = '<span style="color:green;">' + data + '</span>';
-//           }
-//         }
+      //       return data;
+      //     },
+      //   },
+      //   { title: 'LTP', data: 'ltp' },
+      //   { title: 'CLOSEP', data: 'closep' },
+      //   {
+      //     title: 'CHANGE', data: 'change',
+      //     render: function (data, type, row) {
+      //       if (type === 'display') {
+      //         if (row.change < 0) {
+      //           data = '<span style="color:red;">' + data + '</span>';
+      //         } else if (row.change == 0) {
+      //           data = '<span style="color:#2a76e8;">' + data + '</span>';
+      //         } else {
+      //           data = '<span style="color:green;">' + data + '</span>';
+      //         }
+      //       }
 
-//         return data;
-//       },
-//     },
-//     { title: 'YCP', data: 'ycp' },
-//     {    
-//       data: null,      
-//       render: function (data, type, row) {
-//         if (type === 'display') {
-//           data = '<button class="btn btn-primary btn-sm" onclick="remove(\'' + row.trading_code + '\')">Remove</button>';
-//         }
+      //       return data;
+      //     },
+      //   },
+      //   { title: 'YCP', data: 'ycp' },
+      //   {
+      //     title: 'Action',
+      //     data:null,
+      //     render: function (data, type, row) {
+      //       return '<button class="btn btn-primary btn-sm" onclick="remove(\'' + row.trading_code + '\')">Remove</button>';
+      //     }
+      //   },
+        
 
-//         return data;
-//       }
-//     },
-//   ]
-// }
-// }
-remove(company: company) {
-  alert(company.trading_code + " is removed.");
-
-}
+      // ],
+   
 
 
-}
+    }
 
 
 
